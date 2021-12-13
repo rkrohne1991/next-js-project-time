265367d91cf8025dec4082b964218c3bddd5ebab
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-12-13 08:41:59 +0000

    Inserting Comments Into The Database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,11 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://rkrohne:z4uJuMifWjEXu35x@cluster0.k5nne.mongodb.net/events?retryWrites=true&w=majority"
+  );
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -16,15 +22,17 @@ function handler(req, res) {
       return;
     }
 
-    console.log(email, name, text);
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId
     };
 
-    console.log(newComment);
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
     res.status(201).json({ message: "Added comment.", comment: newComment });
   }
 
@@ -36,6 +44,8 @@ function handler(req, res) {
 
     res.status(200).json({ comments: dummyList });
   }
+
+  client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -13,7 +13,7 @@ async function handler(req, res) {
       "mongodb+srv://rkrohne:z4uJuMifWjEXu35x@cluster0.k5nne.mongodb.net/newsletter?retryWrites=true&w=majority"
     );
     const db = client.db();
-    await db.collection("emails").insertOne({ email: userEmail });
+    await db.collection("newsletter").insertOne({ email: userEmail });
     client.close();
 
     res.status(201).json({ message: "Signed up!." });
